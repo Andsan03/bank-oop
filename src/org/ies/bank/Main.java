0d379a6e5020e97ac70a0201a3623d9d9c921a38
Andsan03 <andrewabellosanchez123456@gmail.com>
2024-12-05 09:42:34 +0100

    bank app y main

--- src/org/ies/bank/Main.java
+++ src/org/ies/bank/Main.java
@@ -1,7 +1,18 @@
 package org.ies.bank;
 
+import org.ies.bank.components.AccountsReader;
+import org.ies.bank.components.BankApp;
+import org.ies.bank.components.BankReader;
+import org.ies.bank.components.CustomerReader;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in):
+        CustomerReader customerReader = new CustomerReader(scanner);
+        AccountsReader accountsReader = new AccountsReader(scanner, customerReader);
+        BankReader bankReader = new BankReader(scanner,accountsReader);
+        BankApp bankApp = new BankApp(scanner, bankReader);
     }
 }
---
+++ src/org/ies/bank/components/BankApp.java
@@ -0,0 +1,16 @@
+package org.ies.bank.components;
+
+import java.util.Scanner;
+
+public class BankApp {
+    private final Scanner scanner;
+    private final BankReader bankReader;
+
+    public BankApp(Scanner scanner, BankReader bankReader) {
+        this.scanner = scanner;
+        this.bankReader = bankReader;
+    }
+    public void run () {
+
+    }
+}
